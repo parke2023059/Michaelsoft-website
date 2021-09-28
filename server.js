0662eece0d60b3e2c86d82b43720b7a86abe25d8
parke2023059 <parke2023059@live.ytech.edu>
2021-09-28 13:57:22 -0400

    "fixed"

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.use(express.static('./static'));
+ app.use(express.static('./static'));
 
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
+ //use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
@@ -35,6 +35,31 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
   res.render('pages/about');
 });
+
+app.get('/trey', function(req, res) {
+  res.render('pages/trey');
+});
+
+app.get('/parker', function(req, res) {
+  res.render('pages/parker');
+});
+
+app.get('/leniece', function(req, res) {
+  res.render('pages/bio', {
+    name: "Leniece Bennett",
+    para1: `🎓 Programming student, YTECH. ‘22, Game Development, FSU. ‘26`,
+    para2: `Rhythm game enthusiast, Currently learning Javascript, open to learning many other programming languages!`
+  });
+});
+
+app.get('/logan', function(req, res) {
+  res.render('pages/logan' {
+    name: "Logan Hiller",
+    para1: `🎓 Programming student, YTECH. ‘22, Game Development, FSU. ‘26`,
+    para2: `Rhythm game enthusiast, Currently learning Javascript, open to learning many other programming languages!`
+  });
+});
+
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
