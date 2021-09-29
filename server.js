95532cd9344bb3516e1df4c23b6f7f716fcc4811
Trey <90394867+TFloto@users.noreply.github.com>
2021-09-29 09:53:59 -0400

    team page exists

--- server.js
+++ server.js
@@ -36,5 +36,9 @@ app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
+app.get('/team', function(req, res) {
+  res.render('pages/team');
+});
+
 app.listen(8080);
 console.log('Server is listening on port 8080');
